c0f4e487d436681d8ea0b426bccdbf24d4d24b56
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-05-09 17:25:48 +0200

    aggiunto hover

--- src/components/singolaCard.vue
+++ src/components/singolaCard.vue
@@ -13,15 +13,19 @@
 
 <template>
     <div class="boxcard">
-        <!-- <span v-if="elementoArray.name">
-            {{ elementoArray.name }}
-        </span>
-        <span v-else>
-            {{ elementoArray.title }}
-
-        </span> -->
+        
         <img :src="store.pathImageBig+elementoArray.poster_path" alt="">
-
+        <div class="descrizione">
+            <div>
+                <p v-if="elementoArray.name">
+                    {{ elementoArray.name }}
+                </p>
+                <p v-else>
+                    {{ elementoArray.title }}
+                </p>
+            </div>
+            
+        </div>
 
     </div>
 </template>
@@ -34,10 +38,31 @@
         align-items: center;
         justify-content: center;
         padding: 5px;
+        position: relative;
+        &:hover .descrizione{
+            display: block;
+        }
 
         img {
             width:100%;
             height: 100%;
+        }
+        .descrizione {
+            position: absolute;
+            top: 5px;
+            left: 5px;
+            right: 5px;
+            bottom: 5px;
+            background-color: rgba(0, 0, 0, 0.742);
+            color: white;
+            display: none;
+            div {
+                width: 80%;
+                margin: 0 auto;
+                p {
+                    margin-top: 10px;
+                }
+            }
         }
     }
 
