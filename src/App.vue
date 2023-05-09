4cd6697df54e1358f86e43ef34ec0b9e57de2177
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-05-09 11:23:14 +0200

    struttura

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,16 @@
 </script>
 
 <template>
-  <h1>Ciao mondo</h1>
+  <main>
+
+  </main>
 </template>
 
 <style lang="scss">
 @use './style/main.scss';
+  main {
+    height: 100vh;
+    width: 100%;
+    background-color: #141414;
+  } 
 </style>
